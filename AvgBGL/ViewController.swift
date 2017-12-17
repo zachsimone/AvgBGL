dfff52ddd9ebdb1c796a8550a03b881fb74aca4f
Zach Simone <hi@zachsim.one>
2017-12-17 21:32:37 +1100

    Returning BGL data for today from HealthKit.

--- AvgBGL/ViewController.swift
+++ AvgBGL/ViewController.swift
@@ -40,7 +40,35 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         
+        // Start and end dates
+        let startDate = Date().startOfDay // Start of current day
+        let endDate = Date() // The time now
         
+        let predicate = HKQuery.predicateForSamples(withStart: startDate,
+                                                    end: endDate,
+                                                    options: .strictStartDate)
+        
+
+        let bloodGlucose = HKSampleType.quantityType(forIdentifier: .bloodGlucose)
+        
+        let bglQuery = HKSampleQuery.init(sampleType: bloodGlucose!,
+                                             predicate: predicate,
+                                             limit: HKObjectQueryNoLimit,
+                                             sortDescriptors: nil,
+                                             resultsHandler: { (query, results, error) in
+                                                
+                                                for reading in (results as? [HKQuantitySample])! {
+                                                    
+                                                    let mmol = HKUnit.moleUnit(with: .milli, molarMass: HKUnitMolarMassBloodGlucose)
+                                                    let mmolL = mmol.unitDivided(by: HKUnit.liter())
+                                                    
+                                                    print("Reading as mmol/L \(reading.quantity.doubleValue(for: mmolL)) mmol/L")
+                                                }
+                                                
+        })
+        
+        
+        self.healthStore.execute(bglQuery)
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,5 +77,18 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension Date {
+    var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    var endOfDay: Date? {
+        var components = DateComponents()
+        components.day = 1
+        components.second = -1
+        return Calendar.current.date(byAdding: components, to: startOfDay)
+    }
 }
 
