835819cf6f1bf70f420abe8f6ccfc5b9b6c8b45e
Zach Simone <hi@zachsim.one>
2017-12-17 20:02:50 +1100

    Initial Commit

---
+++ AvgBGL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvgBGL
+//
+//  Created by Zachary Simone on 17/12/17.
+//  Copyright © 2017 Zachary Simone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
