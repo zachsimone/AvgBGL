d8c1246bff29f04fd92005dc19d39018bf1f3163
Zach Simone <hi@zachsim.one>
2017-12-17 21:00:38 +1100

    Added BGL HealthKit permission request.

--- AvgBGL/ViewController.swift
+++ AvgBGL/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
+    
+    var healthStore: HKHealthStore!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Check health data is available
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+            
+            let shareTypes = Set<HKSampleType>()
+            
+            var readTypes = Set<HKObjectType>()
+            readTypes.insert(HKSampleType.quantityType(forIdentifier: .bloodGlucose)!)
+            
+            healthStore.requestAuthorization(toShare: shareTypes, read: readTypes) { (success, error) -> Void in
+                if success {
+                    print("success")
+                } else {
+                    print("failure")
+                }
+                
+                if let error = error { print(error) }
+            }
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
